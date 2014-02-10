b7e59b75f5d7fe261ae450d395e4df141d1dd1b0
Karl McGuinness <kmcguinness@okta.com>
2014-02-09 16:32:38 -0800

    added support for SP flow and enhanced logging

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var express    			= require('express');
+var express    			= require('express'),
+	os                  = require('os'),
     fs         			= require('fs'),
     http       			= require('http'),
     path       			= require('path'),
@@ -11,8 +12,6 @@ var express    			= require('express');
     config              = require('./config.js'),
     SimpleProfileMapper = require('./simpleProfileMapper.js');
 
-// globals
-var app = express();
 
 var argv = require('yargs')
     .usage('Simple IdP\nUsage: $0')
@@ -34,28 +33,17 @@ var argv = require('yargs')
 ;
 
 console.log();
-console.log('Configuration')
-console.log('\tListener Port: ' + argv.port);
-console.log('\tIdP Issuer URI: ' + argv.issuer);
-console.log('\tSP ACS URL: ' + argv.acs);
-console.log('\tSP Audience URI: ' + argv.audience);
-console.log('\tDefault RelayState: ' + argv.relaystate);
+console.log('loading configuration...');
+console.log();
+console.log('Listener Port:\n\t' + argv.port);
+console.log('IdP Issuer URI:\n\t' + argv.issuer);
+console.log('SP ACS URL:\n\t' + argv.acs);
+console.log('SP Audience URI:\n\t' + argv.audience);
+console.log('Default RelayState:\n\t' + argv.relaystate);
 console.log();
 
-
-// all environments
-app.set('port', process.env.PORT || argv.port);
-app.use(express.logger('dev'));;
-app.use(express.urlencoded());
-app.use(app.router);
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-// register idp flow route
-app.get('/idp', samlp.auth({
+// idp options
+var idpOptions = {
   issuer:               argv.issuer,
   cert:                 fs.readFileSync(path.join(__dirname, 'server-cert.pem')),
   key:                  fs.readFileSync(path.join(__dirname, 'server-key.pem')),
@@ -66,12 +54,46 @@ app.get('/idp', samlp.auth({
   RelayState:           argv.relaystate,
   profileMapper:        SimpleProfileMapper,
   getUserFromRequest:   function(req) { return config.user; },
-  getPostURL: function (audience, authnRequestDom, req, callback) { 
-                return callback(null, argv.acs);
-              }
-}));
+  getPostURL:           function (audience, authnRequestDom, req, callback) { 
+                          return callback(null, argv.acs);
+                        }
+}
+
+// globals
+var app    = express();
+var server = http.createServer(app);
+
+// all environments
+app.set('port', process.env.PORT || argv.port);
+app.use(express.logger(':date> :method :url - {:referrer} => :status (:response-time ms)'));
+app.use(express.urlencoded());
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// register idp flow route
+app.get('/',     samlp.auth(idpOptions));
+app.post('/',    samlp.auth(idpOptions));
+app.get('/idp',  samlp.auth(idpOptions));
+app.post('/idp', samlp.auth(idpOptions));
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('IdP server listening on port: ' + app.get('port'));
+console.log('starting server...');
+server.listen(app.get('port'), function() {
+	var address  = server.address(),
+			hostname = os.hostname();
+			baseUrl  = address.address === '0.0.0.0' ? 
+				'http://' + hostname + ':' + address.port :
+				'http://localhost:' + address.port
+  
+  console.log('listening on port: ' + app.get('port'));
+  console.log();
+  console.log('urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST');
+  console.log('\t=> ' + baseUrl + '/idp')
+  console.log('urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect');
+  console.log('\t=> ' + baseUrl + '/idp')
+  console.log();
 });
