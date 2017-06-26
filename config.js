2faa8937b38bd5311fcdc79785351ad90b10430e
Jonathan Felchlin <jonathan@energsavvy.com>
2017-06-26 15:20:01 -0700

    Adding support for select type attributes
    If the attribute config contains an options key, the attribute will display as a select field

--- config.js
+++ config.js
@@ -52,6 +52,12 @@ var metadata = [{
   displayName: 'Groups',
   description: 'Group memberships of the user',
   multiValue: true
+}, {
+  id: "userType",
+  optional: true,
+  displayName: 'User Type',
+  description: 'The type of user',
+  options: ['Admin', 'Editor', 'Commenter']
 }];
 
 module.exports = {
